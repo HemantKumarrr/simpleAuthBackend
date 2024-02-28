2fcac9a1da2fbe65d4028169e2b2fee579231ef1
Hemant <hemantkumar29052001@gmail.com>
2024-02-28 13:02:45 +0530

    cors origin change

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://simple-auth-frontend-one.vercel.app",
     credentials: true,
   })
 );
