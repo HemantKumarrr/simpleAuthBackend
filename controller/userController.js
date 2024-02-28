c3b644ddbbcd55f1adc580841564604cd9afd748
Hemant <hemantkumar29052001@gmail.com>
2024-02-28 14:52:33 +0530

    cookie updated

--- controller/userController.js
+++ controller/userController.js
@@ -5,10 +5,8 @@ module.exports.delete_user = async (req, res) => {
   try {
     const id = req.params.id;
     let data = await User.deleteOne({ _id: id });
-    res.cookie("jwt", "", { maxAge: 1, secure: true,
-      httpOnly: true,
-      domain: 'simple-auth-frontend-one.vercel.app' });
-    res.send({ message: "user delted" });
+    res.cookie("jwt", "", { maxAge: 1, httpOnly: true });
+    res.send({ message: "user deleted" });
   } catch (err) {
     res.status(400).json({ message: err });
   }
