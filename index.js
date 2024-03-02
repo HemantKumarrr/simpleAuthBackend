6c748e19a7a77ee0bb55fb6d442c4dc177f3fe5b
Vignesh Gupta <vighneshgupta32@gmail.com>
2024-03-02 14:54:46 +0530

    add port fallback + docker-compose for DB + env example

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const authRouter = require("./router/authRouter");
 const userRouter = require("./router/userRouter");
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 const connectDB = require("./db/mongoDB");
 connectDB();
