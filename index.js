4ff49d28587a9350ead0539046cc9dd19f047b3c
Hemant <hemantkumar29052001@gmail.com>
2024-03-02 18:58:08 +0530

    redo

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://simpleauthfrontend.onrender.com",
+    origin: "https://simple-auth-frontend-one.vercel.app",
     credentials: true,
   })
 );
